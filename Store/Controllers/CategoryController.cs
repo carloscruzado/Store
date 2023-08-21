2a1a6bd517a2b7672d5087ed4a147ebe654749b5
carloscruzado <carlos570570@gmail.com>
2023-08-21 07:53:00 -0500

    crear y listar Category

---
+++ Store/Controllers/CategoryController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Db;
+
+namespace Store.Controllers
+{
+    [ApiController]
+    [Route("category")]
+    public class CategoryController : ControllerBase
+    {
+        private StoreDBContext _context;
+        public CategoryController(StoreDBContext context)
+        {
+            _context = context;
+        }
+        
+        
+        [HttpGet]
+        [Route("create")]
+        public void CrearCategories() 
+        {
+            _context.Categories.Add(new Category() { 
+            Name = "Mascotas",
+            Description="Comida o articulos paa mascotas",
+            });
+            _context.Categories.Add(new Category()
+            {
+                Name = "Lacteos",
+                Description = "Leche, queso,etc",
+            });
+            _context.Categories.Add(new Category()
+            {
+                Name = "Enbutidos",
+                Description = "mortadela, hotdog,etc",
+            });
+            _context.SaveChanges();
+        }
+
+        [HttpGet]
+        public List<Category> GetCategories() 
+        {
+           return _context.Categories.ToList();
+        
+        }
+
+
+    }
+}
